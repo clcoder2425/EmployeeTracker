a23d0aee66c5458142c09888b81c6390d9ade653
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2023-12-06 14:32:37 -0600

    connected database and added Cfonts function

--- server.js
+++ server.js
@@ -10,3 +10,30 @@ const connection = mysql.createConnection({
     password: "Welcome1",
     database: "employeeTracker_db",
 });
+
+// connect to the database
+connection.connect((err) => {
+    try {
+     console.log("Connected to the database!");
+     // start the application
+     start();
+    } catch (err) {
+     console.log("Unable to connect to the database");
+    }
+ });
+ 
+ // Function to start the application of CFONT 
+ cfonts.say('TL, Inc \nSQL Employee Tracker', {
+     font: 'block',                  // define the font face
+     align: 'center',               // define text alignment
+     colors: ['system'],            // define all colors
+     background: 'transparent',   // define the background color, you can also use `backgroundColor` here as key
+     letterSpacing: 1,           // define letter spacing
+     lineHeight: 0.3,           // define the line height
+     space: true,              // define if the output text should have empty lines on top and on the bottom
+     maxLength: '0',                 // define how many character can be on one line
+     gradient: false,               // define your two gradient colors
+     independentGradient: false,   // define if you want to recalculate the gradient for each new line
+     transitionGradient: false,   // define if this is a transition between colors directly
+     env: 'node'                 // define the environment cfonts is being executed in
+ });
